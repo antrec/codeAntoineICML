8eddba08c0c9b95da9d6a4dcd9e3bd68261087b7
RECANATI Antoine <antoine.recanati@inria.fr>
2018-02-27 15:37:52 +0100

    ..

--- testAffineProjection.m
+++ testAffineProjection.m
@@ -1,11 +1,12 @@
 
 %%
 p = 20;
-sparam = p/2;
+sparam = p;%/2;
 b = 1 + rand(p,1);
 b = sort(b, 'descend');
+b(end-5:end)=0;
 bsum = sum(b);
-a = 1.2*bsum;
+a = 1.5*bsum;
 elnet=0.3;
 % Run convex programming software to perform projection
 cvx_begin
@@ -16,8 +17,20 @@ cvx_begin
         y >= 0;
 cvx_end
 
-myy = projconssparse(b,a,floor(p/2));
+sparam = min(sparam, nnz(b));
+myy = projconssparse(b,a,sparam);
+figure; plot(y,'r+'); hold on; plot(myy, 'kx'); hold on; plot(b,'o'); %hold on; plot(xslope,'gd');
+%%
+lambdas = 1./(1:p).^2;
+sumslope=-inf;
+mu = 1e-2;
+while sumslope < a*(1-1e-3)
+    mu = 1.3*mu;
+%     lambdas = 0.5*lambdas;
+% lambdas = ones(
+    [xslope,info] = Adlas(eye(p),b,lambdas);
+    sumslope = sum(xslope);
+end
+%%
 
-slop = [x,info] = Adlas(A,b,lambda,options)
-
-figure; plot(y,'r+'); hold on; plot(myy, 'kx'); hold on; plot(b,'o'); %hold on; plot(myy1,'gd');
+figure; plot(y,'r+'); hold on; plot(myy, 'kx'); hold on; plot(b,'o'); %hold on; plot(xslope,'gd');
